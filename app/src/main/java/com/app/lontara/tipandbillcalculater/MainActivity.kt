ac097ead5aab1b9a97fe50219629f27fcb333dd2
Muhammad Noman <muhammadnoman806372@gmail.com>
2024-05-17 09:17:31 +0500

    First Commit

---
+++ app/src/main/java/com/app/lontara/tipandbillcalculater/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.app.lontara.tipandbillcalculater
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.lontara.tipandbillcalculater.ui.theme.CeruleanBlue
+import com.app.lontara.tipandbillcalculater.ui.theme.PeacockBlue
+import com.app.lontara.tipandbillcalculater.ui.theme.TipAndBillCalculaterTheme
+import java.text.DecimalFormat
+
+
+//        Made by Muhammad Noman
+//
+//        If you need my service or you have any question then you can contact with me.
+//
+//        WhatsApp number :  +923104881573
+//
+//        LinkedIn account :  https://www.linkedin.com/in/muhammad-noman59
+//
+//        Facebook account :  https://www.facebook.com/profile.php?id=100092720556743&mibextid=ZbWKwL
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TipAndBillCalculaterTheme {
+
+                MyApp()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyApp() {
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+            Calculation()
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
